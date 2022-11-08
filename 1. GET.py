2a03a4290a5735b5230a1fb7fc56ed31f2d10936
Tristan <75189508+realTristan@users.noreply.github.com>
2022-11-07 19:03:38 -0500

    Create 1. GET.py

---
+++ 1. GET.py
@@ -0,0 +1,19 @@
+# // Use the requests library
+import requests
+
+# // The url you want to send a request to
+url = "http://httpbin.org/get"
+
+# // Send the http request
+r = requests.get(url)
+
+# // Print the response status code
+# // 200 == Success
+# // 500 == Internal Error
+# // 404 == Page not Found
+# // etc.
+print(r.status_code)
+
+
+# // Print the response body
+print(r.content)
