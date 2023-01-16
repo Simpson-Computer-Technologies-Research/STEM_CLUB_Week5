8fb6b01fefa69700c8662f14223bec1866149fb8
Tristan <75189508+realTristan@users.noreply.github.com>
2023-01-16 09:59:16 -0500

    Update 6. Async.py

--- 6. Async.py
+++ 6. Async.py
@@ -19,3 +19,11 @@ async def my_function_2(num: int):
 
 # // Use asyncio to call the async function
 asyncio.run(my_function(1))
+
+# // You can only use asyncio.run on ONE function
+# // e.g calling asyncio.run(my_function(1)) twice:
+# //
+# // asyncio.run(my_function(1))
+# // asyncio.run(my_function(1))
+# //
+# // Is not a good practice!
