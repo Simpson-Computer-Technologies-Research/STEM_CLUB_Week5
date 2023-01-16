ebeb091ebd1569e1dcf625905c7ecd604af2d4a7
Tristan <75189508+realTristan@users.noreply.github.com>
2023-01-16 09:51:36 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+# // Welcome to STEM Club!
+
+# // Go to https://replit.com/signup and create a new account
+
+# // Create a new project in Python: https://replit.com/new/python3
+
+# // If you can’t create an account, visit https://replit.com/languages/python3 for now.
+
+# // Visit https://forms.gle/ZtcjMo5qPq8kEQCNA to review today's lesson
+
+# // Ignore this!!
+
+from _1_get import *
+#from _2_post import *
+#from _3_put import *
+#from _4_delete import *
+#from _5_threading import *
+#from _6_async import *
+#from _7_project_example import *
+#from _8_speed import *
